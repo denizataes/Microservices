662de28ec4b1c3aaad2d901c06159dd1ea2da400
Deniz Ata EŞ <denizataes@hotmail.com>
2024-01-31 01:45:58 +0300

    initial commit
    commit

---
+++ Microservices/Services/Catalog/FreeCourse.Services.Catalog/Dto/FeatureDto.cs
@@ -0,0 +1,8 @@
+﻿namespace FreeCourse.Services.Catalog.Dto
+{
+    internal class FeatureDto
+    {
+        public int Duration { get; set; }
+
+    }
+}
---
+++ Microservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -0,0 +1,39 @@
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
+var databaseSettingsSection = builder.Configuration.GetSection("AppSettings:DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(databaseSettingsSection);
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<DatabaseSettings>>().Value;
+    }
+);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
